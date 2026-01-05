ffd56d030dafbd2084e0f822529efc27102708b7
himraj123456789 <39591573+himraj123456789@users.noreply.github.com>
2026-01-05 18:31:41 +0530

    Update ugc.py

--- ugc.py
+++ ugc.py
@@ -321,7 +321,7 @@ if st.button("Get Marks"):
     st.dataframe(
         styled_df,
         use_container_width=True,
-        height=200
+        height=700
     )
     st.download_button(
         "⬇️ Download Question-wise Analysis (CSV)",
@@ -329,6 +329,7 @@ if st.button("Get Marks"):
         "ugc_net_question_analysis.csv",
         "text/csv"
     )
+
 
 
 
